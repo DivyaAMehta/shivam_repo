4652dafa0d15341b23079437e31476b410afdf6a
Shivam <Shivam.Yadav@emtecinc.com>
2022-11-16 21:29:30 +0530

    .net.net6 Assignment 3

---
+++ Assignment3/ArithmeticCoreLib/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ArithmeticCoreLib
+{
+    public class Class1
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public int Sub(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public int Mul(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public int Div(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ Assignment3/WebCoreCalculator/Controllers/WeatherForecastController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebCoreCalculator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("ApiController")]
+        public IEnumerable<int> Get()
+        {
+            ArithmeticCoreLib.Class1 calcOperations = new ArithmeticCoreLib.Class1();
+            return new int[] { calcOperations.Add(10,20),calcOperations.Sub(10,20),calcOperations.Mul(10,20),calcOperations.Div(10,20) };
+
+        }
+    }
+}
