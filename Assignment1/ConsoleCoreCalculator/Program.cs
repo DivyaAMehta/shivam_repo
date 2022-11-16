1482131b46e0844c9259d413ae4573f0eb2b70b3
Shivam <Shivam.Yadav@emtecinc.com>
2022-11-16 21:16:14 +0530

    .net.net6 Assignment 1

---
+++ Assignment1/ConsoleCoreCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleCoreCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 3;
+            ArithmeticOperation.add(num1, num2);
+            ArithmeticOperation.sub(num1, num2);
+            ArithmeticOperation.mul(num1, num2);
+            ArithmeticOperation.div(num1, num2);
+        }
+    }
+}
---
+++ Assignment1/ConsoleFrameworkCalculator/ArithmeticOperation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFrameworkCalculator
+{
+    internal class ArithmeticOperation
+    {
+        public static void add(int num1,int num2)
+        {
+            int res=num1+ num2;
+            Console.WriteLine("Addition: " + res);
+        }
+        public static void sub(int num1, int num2)
+        {
+            int res = num1 - num2;
+            Console.WriteLine("Substraction: " + res);
+        }
+        public static void mul(int num1, int num2)
+        {
+            int res = num1 * num2;
+            Console.WriteLine("Multiplication: " + res);
+        }
+        public static void div(int num1, int num2)
+        {
+            int res = num1 / num2;
+            Console.WriteLine("Division: " + res);
+        }
+    }
+}
