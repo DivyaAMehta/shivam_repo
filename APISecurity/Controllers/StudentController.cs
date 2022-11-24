48d25ef51cdb05465abf2cfaf642074868d7fd3a
Shivam <Shivam.Yadav@emtecinc.com>
2022-11-24 12:35:51 +0530

    Web API Security

---
+++ APISecurity/Controllers/StudentController.cs
@@ -0,0 +1,44 @@
+﻿using APISecurity.Models;
+using APISecurity.Repostory;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APISecurity.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly StudentRepo _studentrepo;
+
+        public StudentController(IConfiguration configuration)
+        {
+            _studentrepo = new StudentRepo(configuration);
+        }
+        [Authorize,Authorize]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_studentrepo.Get());
+        }
+        
+        [HttpPost,Authorize]
+        public IActionResult Create(Student student)
+        {
+            return Ok(_studentrepo.Create(student));
+        }
+
+
+        [HttpPut,Authorize]
+        public IActionResult Put(Student student, int StudId)
+        {
+            return Ok(_studentrepo.Update(student, StudId));
+        }
+
+        [HttpDelete,Authorize]
+        public IActionResult Delete(int StudId)
+        {
+            return Ok(_studentrepo.Delete(StudId));
+        }
+    }
+}
